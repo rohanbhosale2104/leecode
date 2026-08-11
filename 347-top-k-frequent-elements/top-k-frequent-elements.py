2d04f15612666165114f76ede339060aa7f4f3e6
Rohan Bhosale <bhosalerohan095@gmail.com>
2026-08-11 11:04:00 +0530

    Time: 14 ms (11.11%) | Memory: 24.7 MB (26.41%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,29 +1,54 @@
-import heapq
+# import heapq
 
+# class Solution:
+#     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+
+#         # 1. Count frequency
+#         freq = {}
+
+#         for num in nums:
+#             freq[num] = freq.get(num, 0) + 1
+
+#         # 2. Min heap
+#         heap = []
+
+#         for num, count in freq.items():
+#             heapq.heappush(heap, (count, num))
+
+#             # Keep only k elements
+#             if len(heap) > k:
+#                 heapq.heappop(heap)
+
+#         # 3. Extract elements
+#         result = []
+
+#         while heap:
+#             count, num = heapq.heappop(heap)
+#             result.append(num)
+
+#         return result
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-
-        # 1. Count frequency
+        # Count frequency of each number
         freq = {}
-
         for num in nums:
             freq[num] = freq.get(num, 0) + 1
 
-        # 2. Min heap
-        heap = []
+        # buckets[i] contains numbers that appear i times
+        buckets = [[] for _ in range(len(nums) + 1)]
 
         for num, count in freq.items():
-            heapq.heappush(heap, (count, num))
+            buckets[count].append(num)
 
-            # Keep only k elements
-            if len(heap) > k:
-                heapq.heappop(heap)
-
-        # 3. Extract elements
+        # Collect from highest frequency to lowest
         result = []
 
-        while heap:
-            count, num = heapq.heappop(heap)
-            result.append(num)
+        for i in range(len(buckets) - 1, 0, -1):
+            for num in buckets[i]:
+                result.append(num)
+
+                if len(result) == k:
+                    return result
 
         return result
+        
